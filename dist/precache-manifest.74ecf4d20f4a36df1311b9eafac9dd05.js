5a834883c3e7fa83e89d3311972b7bbebc3e54e9
Paulo Diogo <paulo.diogo@mercedes-benz.io>
2021-04-02 01:53:44 +0100

    fix: added dist folder as workaround for ts problem

---
+++ dist/precache-manifest.74ecf4d20f4a36df1311b9eafac9dd05.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05354e85945ea3e7591d075e502742a",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "9e44526e503e274415693ff890617c3e",
+    "url": "android-chrome-384x384.png"
+  },
+  {
+    "revision": "b459ce6c0177bb377f9c31c10a370a9a",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "0e0ef179b1ffb7dfe116",
+    "url": "css/app.1bd21e6a.css"
+  },
+  {
+    "revision": "76e51d5a6affcc76c3224e800704ac74",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "fd40d2326c0e56ffaf536b28f4f64994",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "8082b713e2245cfeba9b917824a6e4af",
+    "url": "index.html"
+  },
+  {
+    "revision": "0e0ef179b1ffb7dfe116",
+    "url": "js/app.4e0df39d.js"
+  },
+  {
+    "revision": "903fa700efd7bac4a27a",
+    "url": "js/chunk-vendors.49221be3.js"
+  },
+  {
+    "revision": "e42fbdc0ac2f513510448976a048fc5b",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "e66ae5eed9bcb938be672afa73ecca46",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "e21a15379c69192851a478e373980c58",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "4f213672a5ab70e86129fa7ef19e1ad3",
+    "url": "site.webmanifest"
+  }
+]);
