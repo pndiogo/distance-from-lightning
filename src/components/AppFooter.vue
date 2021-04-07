62c626984a3f2eec4894a1af7ebb612c20d5e08a
Paulo Diogo <paulo.diogo@mercedes-benz.io>
2021-04-07 22:17:06 +0100

    refactor: better components separations and minor css fixes

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer animate__animated animate__fadeIn">
+    <a class="footer__link" href="https://github.com/pndiogo/distance-from-lightning" target="_blank">
+      <img class="footer__logo" src="../assets/images/github.png" alt="GitHub">
+    </a>
+    <a class="footer__link" href="https://www.linkedin.com/in/pndiogo/" target="_blank">
+      <img class="footer__logo" src="../assets/images/linkedin.png" alt="LinkedIn">
+    </a>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  background-color: transparent;
+
+  &__link {
+    padding: 1rem;
+  }
+
+  &__logo {
+    display: block;
+    width: 4rem;
+
+    @media (min-width: $tablet) {
+      width: 5rem;
+    }
+  }
+}
+</style>
